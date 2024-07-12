c6a7a1f6de757e4a97fd62fdf4ca9729eed06759
installer <installer@gmail.com>
2024-07-12 15:14:20 +0100

    better script

--- scripts/dev_make.nix
+++ scripts/dev_make.nix
@@ -3,21 +3,22 @@
 pkgs.writeShellScriptBin "dev" ''
   # Check if at least one argument is provided
   if [ $# -lt 1 ]; then
-    echo "Usage: $0 ENV [NEW_PROJECT_DIRECTORY]"
+    echo """Usage: dev ENV [NEW_PROJECT_DIRECTORY] 
+        
+            Help:
+                   Uses dev templates to setup a project quickly
+                   See full list of templates here: https://github.com/the-nix-way/dev-templates
+        """
     exit 1
   fi
 
-  # Assign the first argument to ENV
-  ENV=$1
-
   # Check if the second argument is provided
   if [ $# -eq 2 ]; then
-    NEW_PROJECT_DIRECTORY=$2
-    # Run the command with two inputs
-    nix flake init --template github:the-nix-way/dev-templates#${ENV} ${NEW_PROJECT_DIRECTORY}
-  else
-    # Run the command with one input
-    nix flake init --template github:the-nix-way/dev-templates#${ENV}
+    mkdir $2
+    cd $2
   fi
+
+  # Run the command with one input
+  nix flake init --template github:the-nix-way/dev-templates#$1
 ''
 
